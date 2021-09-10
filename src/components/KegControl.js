448ff7258ebad9129cb6684e397dd2fc46b7e0c9
mireie <michael.reiersgaard@gmail.com>
2021-09-10 16:27:04 -0700

    add deleting keg

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -77,12 +77,22 @@ export default class KegControl extends React.Component {
     }
   }
 
+  handleDeletingKeg = (id) => {
+    const newMainKegList = this.state.mainKegList.filter(keg => keg.id !== id)
+    this.setState({
+      mainKegList: newMainKegList,
+      selectedKeg: null
+    })
+  }
+
   render() {
     let currentlyVisibleState = null
     let buttonText = null;
 
     if (this.state.selectedKeg != null) {
-      currentlyVisibleState = <KegDetail keg={this.state.selectedKeg} />
+      currentlyVisibleState = <KegDetail
+        keg={this.state.selectedKeg}
+        onClickingDelete={this.handleDeletingKeg} />
       buttonText = "Return to Keg List"
     } else if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewKegForm onNewKegCreation={this.handleAddingNewKegToList} />
--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -1,21 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types"
+import { Button, Row, Col } from 'react-bootstrap'
 
 export default function KegDetail(props) {
-  const { keg } = props;
+  const { keg, onClickingDelete } = props;
   return (
     <React.Fragment>
-      <h1 className="display-1">{keg.name} - {keg.brand}</h1>
-      <ul>
-        <li>Brewry: {keg.brand}</li>
-        <li>ABV: {keg.abv}%</li>
-        <li>Quantity: {keg.qty} pints remain</li>
-        <li>ID: {keg.id}</li>
-      </ul>
+      <Row>
+        <h1 className="display-1">{keg.name} - {keg.brand}</h1>
+        <Col>
+          <h2 className="display-2">Details</h2>
+          <ul>
+            <li>Brewry: {keg.brand}</li>
+            <li>ABV: {keg.abv}%</li>
+            <li>Quantity: {keg.qty} pints remain</li>
+            <li>ID: {keg.id}</li>
+          </ul>
+        </Col>
+        <Col
+          className="alert-danger">
+          <h2 className="display-2">Danger Zone</h2>
+          <Row>
+            <Col></Col>
+            <Col>           
+              <Button
+                onClick={() => onClickingDelete(keg.id)}
+                className="btn-danger py-3 px-5">
+                Delete
+              </Button>
+            </Col>
+            <Col></Col>
+          </Row>
+        </Col>
+      </Row>
     </React.Fragment>
   )
 }
 
 KegDetail.propTypes = {
-  keg: PropTypes.object
+  keg: PropTypes.object,
+  onClickingDelete: PropTypes.func
 }
